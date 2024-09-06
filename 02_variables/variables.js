6233837e91f0049f75acea0ae15e54d26d11c36d
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-09-06 07:09:24 +0000

    "Getting started with variables"

---
+++ 02_variables/variables.js
@@ -0,0 +1,18 @@
+let accountName = "Ashutosh" 
+
+var accountType = "Saving"
+
+const accountNO = 122334
+
+console.log(accountName,accountNO,accountType);
+
+accountName = "Rai" 
+
+accountType = "Current"
+
+//accountNO = 122334
+
+console.table([accountName,accountNO,accountType]);
+
+//console.table
+
