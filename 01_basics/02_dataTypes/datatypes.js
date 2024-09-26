ee815010bbf174ded7625a3c4ec81682704f518c
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-09-26 19:24:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes/datatypes.js
@@ -0,0 +1,14 @@
+//Data types in js
+
+let name = "Ashutosh" //string
+
+let age = 21 //int or bigint
+
+let isLogged = false //bool
+
+//null => standalone value type is object
+
+//undefined => not declare
+
+console.log(typeof null); //type is null
+console.log(typeof ''); 
