ea80128680b5ef7868f5570757e465568538f126
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-09-27 05:15:12 +0000

    Data conversion

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+let accountName = "Ashutosh" 
+
+var accountType = "Saving"
+
+/* we will not use var bcz in limitation of block scope and function scope*/
+
+const accountNO = 122334
+
+console.log(accountName,accountNO,accountType);
+
+accountName = "Rai" 
+
+accountType = "Current"
+
+//accountNO = 122334
+
+console.table([accountName,accountNO,accountType]);
+
+//console.table
+
---
+++ 01_basics/03_conversionTypes.js
@@ -0,0 +1,11 @@
+let age = -33.09
+let isNumber = false
+
+let stringNo = Number(age)
+let stringBool = Boolean(isNumber)
+let numBool = Number(isNumber)
+let numBool1 = Boolean(age)
+
+// console.log(age)
+// console.log(stringBool)
+//console.log(numBool1)
