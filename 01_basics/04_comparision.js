8abd428c74b0a974500668fb01129c65f591527b
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-10-01 10:05:03 +0000

    comparision in data in js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log("2" > 1);
+// console.log("2" < 1);
+// console.log("1" >= 1);
+// console.log("3" == 2+1);
+
+console.log(null >= 1);
+console.log(null <= 1);
+console.log(null == 0);
+
+
+
+
