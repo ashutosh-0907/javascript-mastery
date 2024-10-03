4a56fa5e8a634a7bff12c2618b32eaf889543b19
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-10-03 09:06:33 +0000

    string in js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+let name = "Ashu tosh"
+let surName = "Rai"
+let profession = "Developer"
+
+//console.log(name + surName + "Engineer")
+//console.log(`My name is ${name} and I am ${profession}`);
+
+let name1 = new String("Ashutosh")
+
+console.log(name.toUpperCase());
+
+console.log(name1.toLocaleUpperCase());
+
+
