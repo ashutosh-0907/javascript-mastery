d20c3e432b862cf5c07d38f051785196c203e933
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-10-04 11:11:04 +0000

    Math and number in js

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,25 @@
+let marks = 88.246
+
+//console.log(typeof(marks))
+
+let score = new Number(88.2)
+// console.log(score);
+
+// console.log(score.toString());
+// console.log(score.toFixed(2));
+// console.log(score.toPrecision(5));//2 -> 88, 1->9e.., 3-> 88.2, 4-> 88.20 
+
+//*********** Maths ***********
+
+// console.log(Math.round(4.5));
+// console.log(Math.floor(-4.5567)); //4
+// console.log(Math.ceil(4.001)); //5
+
+let min =10
+let max =20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
