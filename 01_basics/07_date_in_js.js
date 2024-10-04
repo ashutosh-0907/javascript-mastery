75a548d19c7e66ad0cd80af61ce1d1e7de79416e
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-10-04 14:16:40 +0000

    time and date in js

---
+++ 01_basics/07_date_in_js.js
@@ -0,0 +1,14 @@
+let myDate = new Date() //date is object
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString()); //yyyy-mm-dd time
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2001, 9, 16)
+//console.log(myCreatedDate.toDateString());
+
+let curDate = Date.now()
+console.log(curDate.getTime());
+
+
