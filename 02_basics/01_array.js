1df4eb18ec92134ed2dfa6c7b52bc745ff57828f
Ashutosh Rai <113530715+ashutosh-0907@users.noreply.github.com>
2024-10-05 16:12:31 +0000

    Array in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,22 @@
+
+const myArr = [0, 1, 2, 3, 4, 5] //can have mixed datatype
+
+// myArr.push(6)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+// myArr.unshift(10)
+// myArr.shift()
+// console.log(myArr);
+
+//console.log(myArr.includes(4)); // return bool the presence of ele
+console.log(myArr.slice(1,3));
+console.log(myArr);
+
+
+console.log(myArr.splice(1,3));
+console.log(myArr);
+
+
+
+
---
+++ 02_basics/02_array.js
@@ -0,0 +1,18 @@
+let bTech = ["Ashu", "OmG", "Ro", "Nikku"]
+let bPharma = ["Nau","Thakur","Cobra","Mishra"]
+
+// bTech.push(bPharma)
+
+// console.log(bTech);
+// console.log(bTech[4][2]);
+
+// const mjp = bTech.concat(bPharma)
+// console.log(mjp);
+
+// const mjp = [...bTech, ...bPharma] //spread operator
+// console.log(mjp);
+
+const arr = [1,2,[3,4,[5,6,[6,7,[8,9,0]]]]]
+const aarr = arr.flat(Infinity)
+console.log(aarr);
+
